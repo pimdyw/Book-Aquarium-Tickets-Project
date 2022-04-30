a78b3be4845f855ba6035dba94680580fa4c9c87
nutcha20 <Yume23@gmail.com>
2022-04-30 15:21:35 +0700

    update front

--- myfrontend/src/views/PayfinishPage.vue
+++ myfrontend/src/views/PayfinishPage.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
-        <p>Finish Payment...</p>
-    </div>
+  <div class="hero is-fullheight" style="background-color: #1A132F;">
+            <div style="margin: 6rem;">
+                <p class="is-size-2	has-text-white">สำเร็จ</p>
+                <div class="is-large mt-5 p-4" style="background-color: #EBEBFF;">
+                    <div class="level">
+                        <p class="is-size-5 level-left">บัตรชมเที่ยวเดียวออนไลน์ถูกกว่า</p>
+                        <strong class="is-size-5 level-right has-text-link">สำเร็จแล้ว</strong>
+                    </div>
+                    <div class="level">
+                        <img src="#" alt="รูปตั๋ว" width="200em" height="100em" class="image">
+                        <div>
+                            <p>เด็ก</p>
+                            <p>ผู้ใหญ่</p>
+                            <p class="mt-3">วันเข้าชม 03-11-2022</p>
+                        </div>
+                        <div style="margin-bottom: 2.5rem;">
+                            <p>x2</p>
+                            <p>x2</p>
+                        </div>
+                        <div>
+                            <p>560฿</p>
+                            <p>660฿</p>
+                            <p class="mt-3 has-text-link">รวมการสั่งซื้อ: ฿2,400</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </template>
 
 <script>
@@ -12,5 +37,10 @@ export default {
   }
 </script>
 
-<style scoped>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300&family=Sarabun:wght@300&display=swap');
+    *{
+        font-family: 'Prompt', sans-serif;
+        font-family: 'Sarabun', sans-serif;  
+    }
 </style>
